78877da5bdc76dfd9a9c32e4b1e349965a51e4ab
Aryan Bhavsar <aryanbhavsar161202@gmail.com>
2024-01-10 14:54:34 +0530

    Add Route from Edit to View

--- src/app/edit-data/edit-data.component.ts
+++ src/app/edit-data/edit-data.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { UserDataService } from '../services/user-data.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-edit-data',
@@ -7,9 +8,10 @@ import { UserDataService } from '../services/user-data.service';
   styleUrls: ['./edit-data.component.css'],
 })
 export class EditDataComponent {
-  constructor(private userdata: UserDataService) {}
+  constructor(private userdata: UserDataService, private router: Router) {}
 
   edittedData(data: any) {
     this.userdata.editInList(data);
+    this.router.navigate(['viewC']);
   }
 }
